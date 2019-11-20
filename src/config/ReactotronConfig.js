53624e516dff5bce5c1b23315b6709661f12f096
Leonardo Barbosa <leonardo.pb@icloud.com>
2019-11-20 08:47:46 -0300

    Reactotron + redux

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,11 @@
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
 
 if (__DEV__) {
   const tron = Reactotron.configure({ host: '192.168.11.10' })
-    .useReactNative()
+    .use(reactotronRedux())
     .connect();
 
-  console.tron = tron;
-
   tron.clear();
+  console.tron = tron;
 }
